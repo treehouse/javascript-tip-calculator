b169dcef34a2260f73bf67ff4fc2071d8820ccdf
dustinusey <dustinusey@me.com>
2022-05-16 15:12:48 -0500

    updated toFixed method for additional info

--- frontend/dist/scripts/app.js
+++ frontend/dist/scripts/app.js
@@ -86,127 +86,7 @@ const splitOptions = {
 }
 
 
-const events = { 
-    billTotal: () => {
-        const input = document.getElementById('bill-total-input');
-        input.addEventListener('keyup', e => {
-            let value = input.value;
-            if (isNaN(value) || value === ' ' || value === '') {
-                input.value = '';
-                toCalculate.bill = 0;
-                showAdditionalInfo(false);
-            } else {
-                toCalculate.bill = Number(value);
-                calculateBill();
-                showAdditionalInfo(true);
-            }
-        })
-    },
-    tipOptions: () => {
-        const tipContainer = document.querySelector('#tip-option-btn-container');
-        const customTipBtn = document.getElementById('custom-tip-btn');
-        tipContainer.addEventListener('click', e => {
-            let tips = document.querySelectorAll('.tip-option-btn-container button');
-            let spans = document.querySelectorAll('.tip-option-number');
-            tips.forEach(tip => {
-                tip.classList.remove('active');
-            });
-            tips.forEach((tip, index) => {
-                if (e.target === tip) {
-                    toCalculate.tip = Number(spans[index].textContent);
-                    toCalculate.customTip = false;
-                    tip.classList.add('active');
-                    customTipBtn.classList.remove('active');
-                    customTipBtn.textContent = 'Custom Tip';
-                    calculateBill();
-                }
-            });
-        });       
-    },
-    customTip: () => {
-        const customTipBtn = document.getElementById('custom-tip-btn');
-        const overlay = document.getElementById('overlay')
-        const closeModal = document.querySelectorAll('.close-modal');
-        const modal_confirmBtn = document.getElementById('confirm-tip');
-        const modal_tipInput = document.getElementById('custom-tip-amount');
-        customTipBtn.addEventListener('click', () => {
-            overlay.classList.toggle('show');
-        });
-        closeModal.forEach(btn => {
-            btn.addEventListener('click', () => {
-                overlay.classList.toggle('show');
-                modal_confirmBtn.classList.add('disabled');
-            })
-        });
 
-        modal_tipInput.addEventListener('keyup', e => {
-            let value = e.target.value;
-            if (isNaN(value) || value === ' ' || value === '') {
-                modal_tipInput.value = '';
-                toCalculate.customTipValue = 0;
-                modal_confirmBtn.classList.add('disabled');
-                toCalculate.customTip = false;
-            } else {
-                toCalculate.customTipValue = Number(value);
-                toCalculate.customTip = true;
-                modal_confirmBtn.classList.remove('disabled');
-                
-            }
-        })
-    },
-    confirmCustomTip: () => {
-        const customTipBtn = document.getElementById('custom-tip-btn');
-        const confirmCustomTipBtn = document.getElementById('confirm-tip');
-        const tipOptionsBtns = document.querySelectorAll('.tip-option');
-        const modal_tipInput = document.getElementById('custom-tip-amount');
-        confirmCustomTipBtn.addEventListener('click', () => {
-            customTipBtn.classList.add('active');
-            customTipBtn.textContent = `Custom Tip ( $${modal_tipInput.value} )`
-            modal_tipInput.value = '';
-            calculateBill();
-            tipOptionsBtns.forEach(btn => {
-                btn.classList.remove('active');
-            });
-        })
-    },
-    splitOptions: () => {
-        let lis = document.querySelectorAll('#split-container li');
-        lis.forEach((li, index) => {
-            li.addEventListener('mouseover', () => {
-                for (let i = 0; i < index; i++) {
-                    lis[i + 1].classList.add('pending');
-                }
-            });
-            li.addEventListener('mouseout', () => {
-                for (let i = 0; i < index; i++) {
-                    lis[i + 1].classList.remove('pending');
-                }
-            })
-        })
-
-        lis.forEach((li, index) => {
-            li.addEventListener('click', () => {
-                toCalculate.split = Number(index + 1);
-                calculateBill();
-                lis.forEach((item) => {
-                    item.classList.remove('active');
-                })
-                li.classList.add('active');
-                for (let i = 0; i < index; i++) {
-                    lis[i].classList.add('active');
-                }
-            })
-        })
-    },
-    // listener for events
-    listen: () => {
-        events.billTotal();
-        events.tipOptions();
-        events.customTip();
-        events.splitOptions();
-        events.confirmCustomTip();
-    }
- }
 
 // function to show additional info
 function showAdditionalInfo(visibility) {
@@ -218,8 +98,6 @@ function showAdditionalInfo(visibility) {
         billInfo.classList.remove('show');
     }
 }
-
-// function for additional info logic
 
 
 // function to calculate bill
@@ -236,16 +114,16 @@ function calculateBill() {
     if (toCalculate.customTip) {
         tip = toCalculate.customTipValue;
 
-        additionalInfo_tipAmount.textContent = tip;
-        additionalInfo_totalWithTip.textContent = bill + tip;
+        additionalInfo_tipAmount.textContent = tip.toFixed(2);
+        additionalInfo_totalWithTip.textContent = (bill + tip).toFixed(2);
 
         final = (bill + tip) / split;
     } else {
         tip = toCalculate.tip / 100;
         tipTotal = bill * tip;
 
-        additionalInfo_tipAmount.textContent = tipTotal;
-        additionalInfo_totalWithTip.textContent = bill + tipTotal;
+        additionalInfo_tipAmount.textContent = tipTotal.toFixed(2);
+        additionalInfo_totalWithTip.textContent = (bill + tipTotal).toFixed(2);
 
         final = (bill + tipTotal) / split;
     }
@@ -255,18 +133,142 @@ function calculateBill() {
     finalOutput.textContent = final.toFixed(2);
 }
 
-
+// handling the application
 const application = {
     build: () => {
         billTotal.UI.build();
         tipOptions.UI.build();
         splitOptions.UI.build();
-    }
+    },
+    events: { 
+        billTotal: () => {
+            const input = document.getElementById('bill-total-input');
+            input.addEventListener('keyup', e => {
+                let value = input.value;
+                if (isNaN(value) || value === ' ' || value === '') {
+                    input.value = '';
+                    toCalculate.bill = 0;
+                    showAdditionalInfo(false);
+                } else {
+                    toCalculate.bill = Number(value);
+                    calculateBill();
+                    showAdditionalInfo(true);
+                }
+            })
+        },
+        tipOptions: () => {
+            const tipContainer = document.querySelector('#tip-option-btn-container');
+            const customTipBtn = document.getElementById('custom-tip-btn');
+            tipContainer.addEventListener('click', e => {
+                let tips = document.querySelectorAll('.tip-option-btn-container button');
+                let spans = document.querySelectorAll('.tip-option-number');
+                tips.forEach(tip => {
+                    tip.classList.remove('active');
+                });
+                tips.forEach((tip, index) => {
+                    if (e.target === tip) {
+                        toCalculate.tip = Number(spans[index].textContent);
+                        toCalculate.customTip = false;
+                        tip.classList.add('active');
+                        customTipBtn.classList.remove('active');
+                        customTipBtn.textContent = 'Custom Tip';
+                        calculateBill();
+                    }
+                });
+            });       
+        },
+        customTip: () => {
+            const customTipBtn = document.getElementById('custom-tip-btn');
+            const overlay = document.getElementById('overlay')
+            const closeModal = document.querySelectorAll('.close-modal');
+            const modal_confirmBtn = document.getElementById('confirm-tip');
+            const modal_tipInput = document.getElementById('custom-tip-amount');
+            customTipBtn.addEventListener('click', () => {
+                overlay.classList.toggle('show');
+            });
+            closeModal.forEach(btn => {
+                btn.addEventListener('click', () => {
+                    overlay.classList.toggle('show');
+                    modal_confirmBtn.classList.add('disabled');
+                })
+            });
+    
+            modal_tipInput.addEventListener('keyup', e => {
+                let value = e.target.value;
+                if (isNaN(value) || value === ' ' || value === '') {
+                    modal_tipInput.value = '';
+                    toCalculate.customTipValue = 0;
+                    modal_confirmBtn.classList.add('disabled');
+                    toCalculate.customTip = false;
+                } else {
+                    toCalculate.customTipValue = Number(value);
+                    toCalculate.customTip = true;
+                    modal_confirmBtn.classList.remove('disabled');
+                    
+                }
+            })
+        },
+        confirmCustomTip: () => {
+            const customTipBtn = document.getElementById('custom-tip-btn');
+            const confirmCustomTipBtn = document.getElementById('confirm-tip');
+            const tipOptionsBtns = document.querySelectorAll('.tip-option');
+            const modal_tipInput = document.getElementById('custom-tip-amount');
+            confirmCustomTipBtn.addEventListener('click', () => {
+                customTipBtn.classList.add('active');
+                customTipBtn.textContent = `Custom Tip ( $${modal_tipInput.value} )`
+                modal_tipInput.value = '';
+                calculateBill();
+                tipOptionsBtns.forEach(btn => {
+                    btn.classList.remove('active');
+                });
+            })
+        },
+        splitOptions: () => {
+            let lis = document.querySelectorAll('#split-container li');
+            lis.forEach((li, index) => {
+                li.addEventListener('mouseover', () => {
+                    for (let i = 0; i < index; i++) {
+                        lis[i + 1].classList.add('pending');
+                    }
+                });
+                li.addEventListener('mouseout', () => {
+                    for (let i = 0; i < index; i++) {
+                        lis[i + 1].classList.remove('pending');
+                    }
+                })
+            })
+    
+            lis.forEach((li, index) => {
+                li.addEventListener('click', () => {
+                    toCalculate.split = Number(index + 1);
+                    calculateBill();
+                    lis.forEach((item) => {
+                        item.classList.remove('active');
+                    })
+                    li.classList.add('active');
+                    for (let i = 0; i < index; i++) {
+                        lis[i].classList.add('active');
+                    }
+                })
+            })
+        },
+        // listener for events
+        listen: () => {
+            application.events.billTotal();
+            application.events.tipOptions();
+            application.events.customTip();
+            application.events.splitOptions();
+            application.events.confirmCustomTip();
+        }
+     }
 }
+
+
+
+
 
 // build 
 application.build();
-
 // events
-events.listen();
+application.events.listen();
 
